76ffdfd35e0895d82c6adb51c8ef9e0f68debdd7
João Roas <jr.roas@gmail.com>
2023-01-11 18:40:18 -0300

    creating and using generic repository

--- Models/Post.cs
+++ Models/Post.cs
@@ -2,11 +2,10 @@ using Dapper.Contrib.Extensions;
 
 namespace Blog.Models
 {
-    [Table("[Category]")]
-    public class Category
+    [Table("[Post]")]
+    public class Post
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Slug { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,59 @@
 ﻿using Blog.Models;
 using Blog.Repositories;
-using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost;Database=Blog;Trusted_Connection=True;Encrypt=False";
 var connection = new SqlConnection(connectionString);
 connection.Open();
 
-ReadUsers(connection);
-ReadRoles(connection);
-ReadTags(connection);
-
+//CreateRole(connection);
+ReadUsersWithRoles(connection);
+//ReadRoles(connection);
+//ReadTags(connection);
+//CreateUser(connection);
 
 connection.Close();
 
 
-static void ReadUsers(SqlConnection connection)
+static void ReadUsersWithRoles(SqlConnection connection)
 {
-    var repository = new Repository<User>(connection);
-    var items = repository.Get();
+    var repository = new UserRepository(connection);
+    var items = repository.GetWithRoles();
 
     foreach (var item in items)
     {
         Console.WriteLine(item.Name);
+        foreach ( var role in item.Roles)
+        {
+            Console.WriteLine($" - {role.Name}");
+        }
     }
+}
+static void CreateUser(SqlConnection connection)
+{
+    var user = new User()
+    {
+        Email = "gabriela@roas.com",
+        Bio = "Minha esposa",
+        Image = "Imagem",
+        Name = "Gabriela Robonato",
+        PasswordHash = "Password",
+        Slug = "esposa"
+    };
+    var repository = new Repository<User>(connection);
+    repository.Create(user);
+    
+}
+
+static void CreateRole(SqlConnection connection)
+{
+    var role = new Role()
+    {
+        Name = "Suporte",
+        Slug = "Support"
+    };
+    var repository = new Repository<Role>(connection);
+    repository.Create(role);
 }
 
 static void ReadRoles(SqlConnection connection)
